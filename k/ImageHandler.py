a77790f1a5358a55269168a1a21b7391580c2982
K <kira.kisel2008@gmail.com>
2024-12-17 20:55:29 +0300

    a

--- k/ImageHandler.py
+++ k/ImageHandler.py
@@ -1,53 +1,53 @@
+from k import ImageProcessor
 from PIL import Image, ImageOps
 import datetime
 
 now = datetime.datetime.now()
 
 class ImageHandler():
+    #функция при создании
+    def __init__(self, filename):
+        self.filename = filename
+        self.img = self.load(f"img\\{self.filename}")
+        self.Img = ImageProcessor.ImageProcessor(self.img)
+        self.format = self.filename.split('.')[1]
+        self.filename = self.filename.split('.')[0]
+
     #загрузка изображения
-    def load(filename):
+    def load(self,filename):
         with Image.open(filename) as img:
             img.load()
         print(f"Изображение {filename} открыто")
         return img
     
     #сохранение изображения
-    def save(img, filename, format):
-        img.save(f"img\\new_{filename.split('.')[0]}.{format}")
+    def save(self):
+        self.img = self.Img.img
+        self.img.save(f"img\\new_{self.filename.split('.')[0]}.{self.format}")
 
     #сохранение изображения c датой    
-    def save_with_data(img, filename, format):
+    def save_with_data(self):
+        self.img = self.Img.img
         date = (now.strftime("%d-%m-%Y"))
-        img.save(f'img\\new_{filename.split(".")[0]}-{date}.{format}')
-    
-    #изменение размера
-    def change_size(img, new_size):
-        try:
-            img = img.resize(new_size)
-            print("Размер изображения изменён")
-            img.show()
-        except:
-            print("Некорректный ввод данных")
-        return img
+        self.img.save(f'img\\new_{self.filename.split(".")[0]}-{date}.{self.format}')
 
     #изменение формата
-    def change_format(format):
-        if(format==1):
-            return '.jpg'
-        elif(format==2):
-            return '.gif'
-        elif(format==3):
-            return '.png'
-        elif(format==4):
-            return '.jpeg'
+    def change_format(self):
+        inp = int(input("1 - jpg\n2 - gif\n3 - png\n4 - jpeg\n"))
+        if(inp==1):
+            self.format = '.jpg'
+        elif(inp==2):
+            self.format = '.gif'
+        elif(inp==3):
+            self.format = '.png'
+        elif(inp==4):
+            self.format = '.jpeg'
         else:
             print("Некорректный ввод данных")
-            return format
     
     #масштабирование до 50%
-    def change_scale(img):
-        img = ImageOps.scale(img,0.5)
-        return img
+    def change_scale(self):
+        self.img = ImageOps.scale(self.img,0.5)
 
 
     
--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-from k import ImageHandler
+from k import ImageHandler, ImageProcessor
 
-filename=input("Введите название изображения с форматом: ")
-print(filename)
-img = ImageHandler.ImageHandler.load(f"img\\{filename}")
-format = filename.split('.')[1]
+def main():
+    f = False
+    try:
+        Image = ImageHandler.ImageHandler(input("Введите название изображения с форматом: "))
+        f = True
+    except:
+        print("Изображение не найдено")
+    
+    while f:
+        c = int(input("=============\n1 - Сохранить изображение\n2 - Сохранить изображение с датой\n3 - Изменить формат изображения\n4 - Масштабировать изображение до 50%\n5 - Добавление фильтра EMBOSS\n6 - Изменить размер изображения\n7 - Добавить водяной знак\n=============\n"))
+        if c == 1:
+            ImageHandler.ImageHandler.save(Image)
+            exit()
 
-filename=filename.split('.')[0]
+        elif c == 2:
+            ImageHandler.ImageHandler.save_with_data(Image)
+            exit()
+    
+        elif c == 3: 
+            ImageHandler.ImageHandler.change_format(Image)
+        
+        elif c == 4: 
+            ImageHandler.ImageHandler.change_scale(Image)
+    
+        elif c == 5: 
+            Image.img = ImageProcessor.ImageProcessor.add_filter(Image)
 
-new_img = ImageHandler.ImageHandler.change_size(img, 300)
+        elif c == 6: 
+            Image.img = ImageProcessor.ImageProcessor.change_size(Image)
 
+        elif c == 7: 
+            Image.img = ImageProcessor.ImageProcessor.watermark(Image)
+        
+        else: 
+            print('Введите корректное значение')
 
-#filename=ImageHandler.ImageHandler.change_format(filename, 2)
-ImageHandler.ImageHandler.save_with_data(img, filename, format)
-
-
-img.show()
-
+if __name__=="__main__":
+    main()
